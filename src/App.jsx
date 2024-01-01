f72d438462de0dcf5fcbbe25c1e45d899bbab3df
miawithcode <cuttontail@gmail.com>
2024-01-01 21:22:53 +0800

    feat: add first review

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from "react";
-import reviews from "./data"
+import { useState } from 'react';
+import reviews from './data';
+
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const App = () => {
   const [index, setIndex] = useState(0);
-  const {name, job, image, text} = reviews[index];
-
-  console.log(name); // Check if data is succesfully imported
+  const { name, job, image, text } = reviews[index];
 
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <main>
+      <article>
+        <div>
+          <img src={image} width={'150px'} alt={name} />
+          <span><FaQuoteRight /></span>
+        </div>
+        <h4>{name}</h4>
+        <p>{job}</p>
+        <p>{text}</p>
+      </article>
+    </main>
+  );
+};
+export default App;
